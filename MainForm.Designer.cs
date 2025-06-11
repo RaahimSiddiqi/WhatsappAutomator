92941fc3a1fae5fbc9dbd1f9b4f4abca680b89ed
RaahimSiddiqi <k200146@nu.edu.pk>
2025-06-12 00:03:55 +0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WhatsAppAutomator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnLoadExcel = new System.Windows.Forms.Button();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.AccessibleName = "btnStart";
+            this.btnStart.Location = new System.Drawing.Point(58, 134);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(117, 41);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Launch";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.AccessibleName = "txtLog";
+            this.txtLog.Location = new System.Drawing.Point(58, 266);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(683, 208);
+            this.txtLog.TabIndex = 3;
+            // 
+            // btnLoadExcel
+            // 
+            this.btnLoadExcel.Location = new System.Drawing.Point(58, 84);
+            this.btnLoadExcel.Name = "btnLoadExcel";
+            this.btnLoadExcel.Size = new System.Drawing.Size(117, 44);
+            this.btnLoadExcel.TabIndex = 4;
+            this.btnLoadExcel.Text = "Load Excel File";
+            this.btnLoadExcel.UseVisualStyleBackColor = true;
+            this.btnLoadExcel.Click += new System.EventHandler(this.btnLoadExcel_Click_1);
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AccessibleName = "lblPrompt";
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrompt.Location = new System.Drawing.Point(284, 19);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(227, 20);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "WhatsAppNumber Automator";
+            this.lblPrompt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Log:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 76);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(356, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Message (Paragraphs will be sent as separate messages)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.AccessibleName = "messageBox";
+            this.MessageBox.Location = new System.Drawing.Point(274, 95);
+            this.MessageBox.MaxLength = 1024;
+            this.MessageBox.Multiline = true;
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MessageBox.Size = new System.Drawing.Size(467, 142);
+            this.MessageBox.TabIndex = 7;
+            this.MessageBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadExcel);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnLoadExcel;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MessageBox;
+    }
+}
+
